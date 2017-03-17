16c10aa3155bb2771c93e5daf429329615ac2b77
Barry <bachma0507@gmail.com>
2017-03-17 17:05:23 -0400

    Updated CEC Info and Training links on homefragment.java

--- bICSI/src/main/java/org/bicsi/canada2014/fragment/HomeFragment.java
+++ bICSI/src/main/java/org/bicsi/canada2014/fragment/HomeFragment.java
@@ -90,7 +90,7 @@ public class HomeFragment extends Fragment {
 		ivCec.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				openInternalWebview("http://www.bicsi.org/m/content.aspx?id=8437");
+				openInternalWebview("http://www.bicsi.org/m/content.aspx?id=8552");
 			}
 		});
 		ivExhall.setOnClickListener(new View.OnClickListener() {
@@ -137,7 +137,7 @@ public class HomeFragment extends Fragment {
 		ivTrainexam.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
-				openInternalWebview("http://www.bicsi.org/m/content.aspx?id=8450");
+				openInternalWebview("http://www.bicsi.org/m/content.aspx?id=8573");
 				
 			}
 		});
